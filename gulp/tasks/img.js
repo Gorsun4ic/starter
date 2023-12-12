221fcf126f1ad13ad742b671a0c0ec8e232dc3f3
gorsun4ic <gorsunfoster@ukr.net>
2023-12-13 01:26:21 +0200

    Fixed images task and added svg sprites

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,16 +1,6 @@
+
 export const img = () => {
 	return app.gulp
 		.src(app.path.src.img)
-		.pipe(
-			app.plugins.plumber(
-				app.plugins.notify.onError({
-					title: 'IMG',
-					message: 'Error: <%= error.message %>',
-				})
-			)
-		)
 		.pipe(app.gulp.dest(app.path.build.img))
-		// .pipe(app.gulp.src(app.path.src.svg))
-		// .pipe(app.gulp.dest(app.path.build.img))
-		.pipe(app.plugins.browsersync.stream())
 }
--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,10 @@ global.app = {
 
 // Import tasks
 import { copy } from './gulp/tasks/copy.js'
+import { img } from './gulp/tasks/img.js'
+import { sprites } from './gulp/tasks/sprites.js'
 import { reset } from './gulp/tasks/reset.js'
 import { html } from './gulp/tasks/html.js'
-import { img } from './gulp/tasks/img.js'
 import { server } from './gulp/tasks/server.js'
 import { scss } from './gulp/tasks/scss.js'
 import { js } from './gulp/tasks/js.js'
@@ -33,7 +34,10 @@ function watcher() {
 
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle)
 
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, img))
+const mainTasks = gulp.series(
+	fonts,
+	gulp.parallel(copy, img, sprites, html, scss, js)
+)
 
 // Build tasks
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
