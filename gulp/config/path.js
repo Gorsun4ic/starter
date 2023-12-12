4ff1f9f479c1d07b57e6c7fc9d72689978a37d7a
gorsun4ic <gorsunfoster@ukr.net>
2023-12-12 20:39:51 +0200

    Added function to copy img, but still fixing

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,10 +1,9 @@
-
 // Get folder name
 // import { src } from 'gulp';
-import * as nodePath from 'path';
-const rootFolder = nodePath.basename(nodePath.resolve());
+import * as nodePath from 'path'
+const rootFolder = nodePath.basename(nodePath.resolve())
 
-const buildFolder = `./dist`;
+const buildFolder = `./dist`
 const srcFolder = `./src`
 
 export const path = {
@@ -12,11 +11,14 @@ export const path = {
 		js: `${buildFolder}/js/`,
 		css: `${buildFolder}/css/`,
 		html: `${buildFolder}/`,
+		img: `${srcFolder}/img/`,
 		fonts: `${buildFolder}/fonts/`,
 		files: `${buildFolder}/files/`,
 	},
 	src: {
 		js: `${srcFolder}/js/app.js`,
+		img: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp}`,
+		svg: `${srcFolder}/svg/**/*.svg`,
 		scss: `${srcFolder}/scss/style.scss`,
 		html: `${srcFolder}/*.html`,
 		files: `${srcFolder}/files/**/*.*`,
@@ -25,6 +27,7 @@ export const path = {
 		js: `${srcFolder}/js/**/*.js`,
 		scss: `${srcFolder}/scss/**/*.scss`,
 		html: `${srcFolder}/**/*.html`,
+		img: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp, svg, ico}`,
 		files: `${srcFolder}/files/**/*.*`,
 	},
 	clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
 // Main module
-import gulp from "gulp";
+import gulp from 'gulp'
 // Import path
-import {path} from "./gulp/config/path.js";
+import { path } from './gulp/config/path.js'
 // Import general plugins
 import { plugins } from './gulp/config/plugins.js'
 
 // Get values in global variable
 global.app = {
-    path: path,
-    gulp: gulp,
-    plugins: plugins
+	path: path,
+	gulp: gulp,
+	plugins: plugins,
 }
 
 // Import tasks
-import { copy } from "./gulp/tasks/copy.js"
+import { copy } from './gulp/tasks/copy.js'
 import { reset } from './gulp/tasks/reset.js'
 import { html } from './gulp/tasks/html.js'
+import { img } from './gulp/tasks/img.js'
 import { server } from './gulp/tasks/server.js'
 import { scss } from './gulp/tasks/scss.js'
 import { js } from './gulp/tasks/js.js'
-import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js'
 
 // File editing watcher
 function watcher() {
-    gulp.watch(path.watch.files, copy);
-    gulp.watch(path.watch.html, html);
-    gulp.watch(path.watch.scss, scss);
-    gulp.watch(path.watch.js, js)
+	gulp.watch(path.watch.files, copy)
+	gulp.watch(path.watch.html, html)
+	gulp.watch(path.watch.scss, scss)
+	gulp.watch(path.watch.js, js)
+	gulp.watch(path.watch.img, img)
 }
 
-const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle)
 
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js))
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, img))
 
 // Build tasks
-const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
 
 // Run default tasks
-gulp.task('default', dev);
+gulp.task('default', dev)
